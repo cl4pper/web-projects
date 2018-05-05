b83acc0a999edf1148058000bbbe216bb7b04b87
cl4pper <cl4pper@gmail.com>
2018-05-05 17:51:04 -0300

    Tasklist: Add: list component, helper to generate list.

---
+++ tasklist/client/list/main.js
@@ -0,0 +1,9 @@
+Template.list.helpers({
+  tasks: function() {
+    return [
+      { name: "Go to the market store"},
+      { name: "Go to the mall"},
+      { name: "Go to work"},
+    ];
+  }
+});
