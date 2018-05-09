7c4c70e46eb6e9e52be081c03eb2b9abf822649a
cl4pper <cl4pper@gmail.com>
2018-05-09 16:09:56 -0300

    Firebaseapp-vue: Add: insert data in db, read data from db.

--- firebaseapp-vue/src/App.vue
+++ firebaseapp-vue/src/App.vue
@@ -1,25 +1,41 @@
 <template>
   <div id="app">
-    <div class="col-sm-5">
+    <div class="col-sm-8">
       <div class="container">
         <div class="input-group mb-3">
           <div class="input-group-prepend">
             <span class="input-group-text">Name</span>
           </div>
-          <input type="text" class="form-control" />
-          <button class="btn btn-success" @click="showAlert">Add</button>
+          <input type="text" class="form-control" v-model="name"/>
+          <button class="btn btn-success" @click="insertName">Add</button>
         </div>
+        <ul>
+          <li v-for="name in dbNames" v-bind:key="name['.key']">{{ name.name }}</li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { namesRef } from './firebase';
+
 export default {
   name: 'app',
+  data() {
+    return {
+      name: "Hello, world",
+    }
+  },
+  firebase: {
+    dbNames: namesRef,
+  },
   methods: {
     showAlert: function() {
-      alert("Button clicked!")
+      alert(this.name)
+    },
+    insertName: function() {
+      namesRef.push({ name: this.name })
     }
   }
 }
--- firebaseapp-vue/src/firebase.js
+++ firebaseapp-vue/src/firebase.js
@@ -1,5 +1,5 @@
-// import { initializeApp } from 'firebase'
-//
+import Firebase from 'firebase';
+
 // const app = initializeApp({
 //   apiKey: "AIzaSyD6SNXF5ehqg_OZ-GfmVDUSyt0ItqzvMfI",
 //   authDomain: "test-vue-84656.fireb aseapp.com",
@@ -8,6 +8,15 @@
 //   storageBucket: "",
 //   messagingSenderId: "797479608538"
 // });
-//
-// export const db = app.databse()
-// export const namesRef = db.ref('names')
+
+const app = Firebase.initializeApp({
+  apiKey: "AIzaSyD6SNXF5ehqg_OZ-GfmVDUSyt0ItqzvMfI",
+  authDomain: "test-vue-84656.firebaseapp.com",
+  databaseURL: "https://test-vue-84656.firebaseio.com",
+  projectId: "test-vue-84656",
+  storageBucket: "test-vue-84656.appspot.com",
+  messagingSenderId: "797479608538"
+});
+
+export const db = app.database();
+export const namesRef = db.ref('names');
