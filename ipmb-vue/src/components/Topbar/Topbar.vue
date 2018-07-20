259dda011a91f5ad59b78057d84e91a570c9742e
cl4pper <cl4pper@gmail.com>
2018-07-20 00:10:32 -0300

    IPMB-vue:
    Create Topbar, Jumbotron, Title(page title) components. Start Subtitle component.

---
+++ ipmb-vue/src/components/Topbar/Topbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="topbar">
+    <!-- ICON TO OPEN TOGGLE MENU -->
+    <p class="icon-open">&#9776;</p>
+
+    <!-- ICON TO CLOSE TOGGLE MENU -->
+    <!-- <scan class="icon-close">&#9747;</scan> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'topbar',
+}
+</script>
+
+<style lang="scss">
+@import './style.scss';
+</style>
