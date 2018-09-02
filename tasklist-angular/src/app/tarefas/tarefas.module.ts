87368b1b87332158cceae55729e4aa6816cd12aa
cl4pper <cl4pper@gmail.com>
2018-09-02 15:16:43 -0300

    tasklist-angular
    tarefa model created

--- tasklist-angular/src/app/tarefas/tarefas.module.ts
+++ tasklist-angular/src/app/tarefas/tarefas.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TarefaService } from './shared';
+
 @NgModule({
   imports: [
     CommonModule
